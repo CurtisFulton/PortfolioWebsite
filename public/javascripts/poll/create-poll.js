c027818722996d031a58ea9c72aa5bc29c5f9cf1
Curtis Fulton <Curtis.R.Fulton@Gmail.com>
2017-06-19 21:09:29 +1000

    Restructure portfolio folder hierarchy
    Completely changed how the folder hierarchy works.
    
    There is an initial server which uses creates a vhost for each sub domain.
    That is all this git project does. It acts as a host for all the other
    projects.
    
    This means that all other projects (Hosted under a sub domain) are
    completely separate.
    Each one has its own dependencies, and has no knowledge of any other
    sites.
    
    I will now be creating a separate git repo for each new project, making it
    much nicer to manage.

--- public/javascripts/poll/create-poll.js
+++
@@ -1,8 +0,0 @@
-
-$(document).on('input', '.pollOption', function() {
-	if ($('.pollOption:last').val()) {
-		var $newOption = $('.pollOption:last').clone(true);
-		$newOption.val(null);
-		$("#pollOptions").append($newOption);
-	}
-});
--- server.js
+++ server.js
@@ -1,44 +1,12 @@
 const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
-const validator = require('validator');
-
 const vhost = require('vhost');
 
 const port = 8080;
 const app = express();
 
-// View engine
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-
-loadExpressValidator();
-
-// Body Parser 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-// Global Vars
-app.use(function(req, res, next) {
-	res.locals.errors = null;
-	next();
-});
-
-
-// Main Controllers
-const home = require('./routes/home');
-const portfolio = require('./routes/portfolio');
-
-app.use(vhost('Poll.curtisfulton.me', require('./sub-sites/poll').app));
-app.use(vhost('NorthForce.curtisfulton.me', require('../NorthForceLandscaping').app));
-
-// Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', home);
-app.use('/portfolio', portfolio);
+app.use(vhost('Www.curtisfulton.me', require('../WebProjects/www').app));
+app.use(vhost('Poll.curtisfulton.me', require('../WebProjects/poll').app));
+app.use(vhost('NorthForce.curtisfulton.me', require('../WebProjects/northforce').app));
 
 // 500 Error
 app.use(function(err, req, res, next) {
@@ -46,7 +14,7 @@ app.use(function(err, req, res, next) {
 	console.log(err);
 });
 
-// 500 Error
+// 404 Error
 app.use(function(req, res) {
 	res.status(404).send("404: Page not found");
 });
@@ -55,48 +23,3 @@ app.use(function(req, res) {
 app.listen(port, function() {
 	console.log('Server Online. Port: ' + port);
 });
-
-
-function loadExpressValidator() {
-	// Express Validator
-	app.use(expressValidator({
-	  errorFormatter: function(param, msg, value) {
-	      var namespace = param.split('.')
-	      , root    = namespace.shift()
-	      , formParam = root;
-
-	    while(namespace.length) {
-	      formParam += '[' + namespace.shift() + ']';
-	    }
-	    return {
-	      param : formParam,
-	      msg   : msg,
-	      value : value
-	    };
-	  }
-	}));
-
-	// Custom Validators
-	app.use(expressValidator({
-		customValidators: {
-			isArray: function(value) {
-				return Array.isArray(value)
-			},
-			validStrings: function(array, value) {
-				var valid = 0;
-				
-				array.forEach(function(val){
-					if (val != null && val.length > 0)
-						valid++;
-				});
-
-				return valid >= value;
-			},
-			eachNotEmpty: function(values) {
-				return values.every(function(val){
-					return val != null && val.length > 0;
-				});
-			}
-		}
-	}));
-}
